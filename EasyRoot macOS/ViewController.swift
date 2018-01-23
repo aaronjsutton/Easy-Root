795293e0b0d6b753f2be10d8d3dfd21bdbc9713c
Aaron Sutton <aaronjsutton@icloud.com>
2018-01-23 10:31:12 -0500

    macOS App Target

---
+++ EasyRoot macOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EasyRoot macOS
+//
+//  Created by Aaron Sutton on 1/23/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
